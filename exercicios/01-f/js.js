ea1f8c29d9497e635956f9b378cf5372b4b6365c
aquino062 <willian_wjunior@hotmail.com>
2023-04-14 08:13:11 -0300

    .

--- exercicios/01-f/js.js
+++ exercicios/01-f/js.js
@@ -1,6 +1,7 @@
 let menu;
 let cinema = [];
 
+// Inicializa o array 'cinema' com 10 linhas e 10 colunas, e preenche cada posição com o valor 'o'
 for (let i = 0; i < 10; i++) {
   cinema[i] = [];
   for (let j = 0; j < 10; j++) {
@@ -8,20 +9,25 @@ for (let i = 0; i < 10; i++) {
   }
 }
 
+// Loop que exibe um menu para o usuário e espera sua escolha
 do {
   menu = prompt('Escolha uma opção:\n1 - Reservar\n2 - Layout do cinema\n3 - Sair');
 
+  // Caso o usuário escolha a opção 1 - Reservar
   if (menu === '1') {
-    let fila = parseInt(prompt('Fila: '));
-    let poltrona = parseInt(prompt('Poltrona: '));
+    let fila = parseInt(prompt('Fila: ')); // Pede para o usuário informar o número da fila
+    let poltrona = parseInt(prompt('Poltrona: ')); // Pede para o usuário informar o número da poltrona
 
+    // Verifica se a poltrona selecionada está disponível
     if (cinema[fila][poltrona] === 'o') {
-      cinema[fila][poltrona] = 'x';
+      cinema[fila][poltrona] = 'x'; // Se estiver disponível, marca a poltrona como ocupada
       console.log('Poltrona reservada!');
     } else {
-      console.log('Poltrona já ocupada!');
+      console.log('Poltrona já ocupada!'); // Se não estiver disponível, exibe uma mensagem de erro
     }
   } else if (menu === '2') {
+    console.log(cinema); // Exibe o array 'cinema' no console
+    // Loop que percorre o array 'cinema' e exibe cada posição no formato de uma matriz
     for (let i = 0; i < 10; i++) {
       let linha = '';
       for (let j = 0; j < 10; j++) {
@@ -32,4 +38,4 @@ do {
   }
 } while (menu !== '3');
 
-console.log(cinema);
+console.log(cinema); // Exibe o array 'cinema' final no console
