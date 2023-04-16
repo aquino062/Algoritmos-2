2868295361274bc00a478cc7883931c582b3dcac
aquino062 <willian_wjunior@hotmail.com>
2023-04-16 11:56:26 -0300

    2.0

--- exercicios/02-d/02.d.js
+++ exercicios/02-d/02.d.js
@@ -5,62 +5,66 @@ let nomeAPesquisar;
 let indiceExclusao;
 
 function mostrarMenu() {
-  opcaomenu = prompt("1- Cadastrar \n2- Pesquisar \n3- Excluir");
+    opcaomenu = prompt(
+        "1- Cadastrar \n2- Pesquisar \n3- Excluir \n4- exibir clientes \n5- sair"
+    );
 }
 
 do {
-  mostrarMenu();
+    mostrarMenu();
 
-  switch (opcaomenu) {
-    case "1":
-      cadastrar();
-      break;
-    case "2":
-      pesquisar();
-      break;
-    case "3":
-      excluir();
-      break;
-    default:
-      console.log("opção inválida");
-      break;
-  }
-} while (opcaomenu !== "4");
+    switch (opcaomenu) {
+        case "1":
+            cadastrar();
+            break;
+        case "2":
+            pesquisar();
+            break;
+        case "3":
+            excluir();
+            break;
 
+        case "4":
+            exibirClientes();
+            break;
+        default:
+            console.log("opção inválida");
+            break;
+    }
+} while (opcaomenu !== "5");
 
 function cadastrar() {
-  let nomeCliente;
-  for (let i = 0; i < clientes.length; i++) {
-    if (clientes[i] === undefined) {
-      nomeCliente = prompt("Informe o nome do cliente");
-      clientes[i] = nomeCliente;
-      break;
+    for (let i = 0; i <= 4; i++) {
+        if (clientes[i] === undefined) {
+            clientes[i] = prompt("Informe o nome do cliente");
+            exibirClientes();
+            break;
+        }
     }
-  }
 }
-
 
 function pesquisar() {
-  nomeAPesquisar = prompt("Informe o nome do cliente a pesquisar");
+    nomeAPesquisar = String(prompt("Informe o nome do cliente a pesquisar"));
 
-  indiceSucesso = clientes.indexOf(nomeAPesquisar);
+    indiceSucesso = clientes.indexOf(nomeAPesquisar);
 
-  if (indiceSucesso === -1) {
-    console.log("Não foi encontrado");
-  } else {
-    console.log("Cliente encontrado na posição " + indiceSucesso);
-  }
+    if (indiceSucesso === -1) {
+        console.log("Não foi encontrado");
+    } else {
+        console.log("Cliente encontrado na posição " + indiceSucesso);
+    }
 }
-
 
 function excluir() {
-  indiceExclusao = prompt("Informe o índice a excluir");
+    indiceExclusao = prompt("Informe o índice a excluir");
 
-  if (indiceExclusao >= 0 && indiceExclusao < clientes.length) {
-    clientes.splice(indiceExclusao, 1);
-  } else {
-    console.log("Índice inválido");
-  }
+    if (indiceExclusao >= 0 && indiceExclusao < clientes.length) {
+        clientes.splice(indiceExclusao, 1);
+    } else {
+        console.log("Índice inválido");
+    }
 }
 
-
+function exibirClientes() {
+    console.log(clientes);
+}
